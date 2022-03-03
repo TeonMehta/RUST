668fdf2623f0bd8e989e98023856649b50022f1c
TeonMehta <teonmehta@gmail.com>
2022-03-03 13:28:59 +0100

    completed hello-world exercise

---
+++ hello-rust/target/debug/deps/hello_rust-bbad7f42d6160482.d
@@ -0,0 +1,5 @@
+/Users/tswsbtd/RUST/hello-rust/target/debug/deps/hello_rust-bbad7f42d6160482: src/main.rs
+
+/Users/tswsbtd/RUST/hello-rust/target/debug/deps/hello_rust-bbad7f42d6160482.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/tswsbtd/RUST/hello-rust/target/debug/hello-rust: /Users/tswsbtd/RUST/hello-rust/src/main.rs
